23f8c2539d857193fa520ee46d706b7e68109906
Adebanjo Abraham <navigatorabraham17@gmail.com>
2025-09-29 10:16:05 +0000

    but use a text font like the image attacahed for the CryptoQuest

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,7 @@ export default {
       fontFamily: {
         body: ['Inter', 'sans-serif'],
         headline: ['Inter', 'sans-serif'],
+        logo: ['Great Vibes', 'cursive'],
         code: ['monospace'],
       },
       colors: {
